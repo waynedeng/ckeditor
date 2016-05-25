8bfdbec60af4c907118ea878dfdb155b8db65a6c
wayne <wayne.deng@rccchina.com>
2016-05-25 17:54:14 +0800

    Update CKEditor to version 4.5.9 and add Uploading Dropped or Pasted Files plugin

--- app/controllers/ckeditor/attachment_files_controller.rb
+++ app/controllers/ckeditor/attachment_files_controller.rb
@@ -10,6 +10,29 @@ class Ckeditor::AttachmentFilesController < Ckeditor::ApplicationController
     end
   end
 
+  def upload_paste_file
+    if file = params[:upload]
+
+      if !file.original_filename.empty?
+        filename = file.original_filename
+        file_ext = filename.split('.').last.downcase
+        new_filename = "#{Time.now.strftime('%Y%m%d%H%M%S')}#{rand(10)}#{rand(10)}#{rand(10)}#{rand(10)}.#{file_ext}"
+        folder_path = "/upload/#{Time.now.strftime('%Y-%m-%d')}"
+        full_folder_path = "#{Rails.root}/public#{folder_path}"
+        FileUtils.mkdir(full_folder_path) unless File.exist?(full_folder_path)
+        
+        File.open("#{full_folder_path}/#{new_filename}", "wb") do |f|
+          f.write(file.read)
+        end
+        render :json=>{url: "#{folder_path}/#{new_filename}", uploaded: 1, 'fileName'=>filename}
+      else
+        render :json=>{error: {message: 'Upload error!'}, uploaded: 0}
+      end
+    else
+      render :json=>{error: {message: 'Upload error!'}, uploaded: 0}
+    end
+  end
+
   def create
     @attachment = Ckeditor.attachment_file_model.new
     respond_with_asset(@attachment)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Ckeditor::Engine.routes.draw do
   resources :pictures, :only => [:index, :create, :destroy]
-  resources :attachment_files, :only => [:index, :create, :destroy]
+  resources :attachment_files, :only => [:index, :create, :destroy] do
+  	collection do
+  		post :upload_paste_file
+  	end
+  end
 end
